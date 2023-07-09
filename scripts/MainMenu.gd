44f7225af9ec32e508f48d8fd8ebe23587ad30d1
gui-vendruscolo <guilherme.vensilva@gmail.com>
2023-07-09 02:04:31 -0300

    alterada resolução

--- scripts/MainMenu.gd
+++ scripts/MainMenu.gd
@@ -12,7 +12,7 @@ func _process(delta):
 
 
 func _on_start_button_pressed():
-	get_tree().change_scene_to_file("res://main_scene.tscn")
+	get_tree().change_scene_to_file("res://scenes/main_scene.tscn")
 	
 func _on_options_button_pressed():
 	pass

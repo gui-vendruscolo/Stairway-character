18f70b9f602184261e53897f01e99793af7c4dc4
LuizTrMr <luizfelipetmoraes@hotmail.com>
2023-07-09 08:20:23 -0300

    Acertos de gameplay

--- scripts/escada.gd
+++ scripts/escada.gd
@@ -9,7 +9,7 @@ enum Dir {UP = -1, DOWN = 1}
 
 
 var step := 10.0
-
+var body_is_climbing := false
 
 func _ready():
 	pass
@@ -17,19 +17,21 @@ func _ready():
 
 func _physics_process(delta):
 	if active:
-		var dir_x = Input.get_axis("ui_left", "ui_right")
-		if dir_x != 0:
-			velocity.x = dir_x * SPEED
-		else:
-			velocity.x = move_toward(velocity.x, 0, SPEED)
-		
 		high_light(true)
-		
-		var dir_y = Input.get_axis("ui_up", "ui_down")
-		velocity.y = dir_y * SPEED
+		if !body_is_climbing:
+			var dir_x = Input.get_axis("ui_left", "ui_right")
+			if dir_x != 0:
+				velocity.x = dir_x * SPEED
+			else:
+				velocity.x = move_toward(velocity.x, 0, SPEED)
+			
+			var dir_y = Input.get_axis("ui_up", "ui_down")
+			velocity.y = dir_y * SPEED
+		else:
+			velocity = Vector2.ZERO
 	else:
-		velocity = Vector2.ZERO
 		high_light(false)
+		velocity = Vector2.ZERO
 
 	move_and_slide()
 
@@ -50,9 +52,11 @@ func _on_area_body_entered(body):
 		body.y_direction = dir
 		body.set_collision_mask_value(2, false)
 		body.is_climbing = true
+		body_is_climbing = true
 
 
 func _on_area_body_exited(body):
 	if body.is_climbing:
 		body.set_collision_mask_value(2,true)
 		body.is_climbing = false
+		body_is_climbing = false
